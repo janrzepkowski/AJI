2eac52159a8b7f9914b64eb3ac946a2dd31d7a4f
Jan Rzepkowski 247777 <jan.rzepkowski21@gmail.com>
2024-11-05 08:48:15 +0100

    Testing connection to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
 
 app.get("/", (req, res) => {
   res.send("Hello from Node API");
 });
+
+mongoose
+  .connect(
+    "mongodb+srv://janrzepkowski21:iRResdFwmxYs6hsr@cluster0.svxj9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    {}
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(3000, () => {
+        console.log("Server is running on port 3000");
+      });
+  })
+  .catch((err) => {
+    console.log("Error: ", err);
+  });
