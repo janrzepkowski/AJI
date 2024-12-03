8cfee094d2c119644df2513a202c046b0b489840
Jan Rzepkowski 247777 <jan.rzepkowski21@gmail.com>
2024-12-03 13:12:37 +0100

    define axios

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 const { StatusCodes } = require("http-status-codes");
 const Product = require("../models/product");
 const {
