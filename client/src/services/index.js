0c419093cfcaeac3fa3fb05f16001bf1461aaf1d
janrzepkowski <jan.rzepkowski21@gmail.com>
2025-03-24 23:36:26 +0100

    Render server

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3001/api";
+const baseUrl = "https://aji-24kf.onrender.com/api";
 
 const login = async (credentials) => {
   const response = await axios.post(`${baseUrl}/login`, credentials, {
